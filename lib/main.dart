2828757d3aad8219d85f89f91ab0440cf0668a63
胡孝飞 <hxfsc2@gmail.com>
2021-04-12 21:39:23 +0800

    feat: tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,68 +8,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Colors.white,
+      home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.bathtub)),
+                  Tab(icon: Icon(Icons.account_balance)),
+                  Tab(icon: Icon(Icons.favorite_border))
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                Icon(Icons.bathtub),
+                Icon(Icons.account_balance),
+                Icon(Icons.favorite_border)
+              ],
+            ),
+          )
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final bingImg1 = "https://cn.bing.com/th?id=OHR.HovenweepDarkSky_ZH-CN5264718531_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4&r=0";
-  final bingImg2 = "https://cn.bing.com/th?id=OHR.TetraoTetrix_ZH-CN3813461274_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4";
-  final bingImg3 = "https://cn.bing.com/th?id=OHR.WillowNewGrowth_ZH-CN3104122677_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4";
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: ListView(
-        children: <Widget>[
-          new Center(child: new Image.network(bingImg1)),
-          new Center(child: new FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: bingImg2)),
-          new Center(child: new CachedNetworkImage(progressIndicatorBuilder: (context, url, downloadProgress)=> new CircularProgressIndicator(value: downloadProgress.progress,), imageUrl: bingImg3))
-        ],
-      )
-    );
-  }
-}
